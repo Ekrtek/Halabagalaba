70fb4ae5602dc1eea082ab2f183f0752b8db0fbe
Ekrtek <ekrtek99@gmail.com>
2025-09-03 23:41:33 +0300

    Lisäys ilta koodauksen mukana

--- luento2_6.py
+++ luento2_6.py
@@ -4,4 +4,9 @@
 from datetime import datetime
 
 today = datetime.today()
+month = datetime.today().month
+print(f"{today.month:02d}")
 print(today)
+
+
+# jos haluaa saada päivä ja kuun kahen numeron formattiin f stringissä {month:02d} tai {day:02d}
