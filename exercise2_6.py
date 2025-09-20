cd255356b958ddd77064bff7b9cb649c3ea15e57
Ekrtek <ekrtek99@gmail.com>
2025-09-20 13:45:02 +0300

    Lisäys ilta koodauksen mukana

--- exercise2_6.py
+++ exercise2_6.py
@@ -1,7 +1,7 @@
 import math
-cherry = 2 + 2 * 2 + 2 - 2 - 2
+cherry = (2 + 2 * 2 + 2 - 2 - 2) / 2
 apple = (math.sqrt(3+10-4) / 3) + ((5 ** 3 - 5) / 20) + 3
 orange = apple - 9
-banana = (-10 + cherry) / 3
+banana = (-10 + cherry * 2) / 3
 pear = -8 + (banana * 3)
-print(apple - (banana*2) + (orange * 2) * (pear + cherry))
+print(apple - (banana * 2) + (orange * 2) * (pear + cherry))
